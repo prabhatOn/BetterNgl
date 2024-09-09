8506f27c935e729e01b0d02f96eba15c79f88491
prabhatOn <prabhat@ssipmt.com>
2024-09-09 22:46:07 +0530

    database connection dbConnect.ts

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import mongoose from 'mongoose';
+
+type ConnectionObject = {
+    isConnected?: number
+}
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+    if (connection.isConnected) {
+        console.log('Using existing connection');
+        return;
+    }
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || '',) //add option after studing mongoose
+        connection.isConnected = db.connections[0].readyState
+        console.log('New DB connection created');
+    } catch (error) {
+        console.error('Error connecting to database', error);
+        process.exit(1);
+    }
+    
+}
+
+export default dbConnect;
