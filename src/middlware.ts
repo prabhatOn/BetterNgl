e3f815fdc5fa874ac64be4a781fe59a5e740b6ac
prabhatOn <prabhat@ssipmt.com>
2024-09-11 01:10:58 +0530

    update middleware if not token then redirect to login page

--- src/middlware.ts
+++ src/middlware.ts
@@ -4,24 +4,27 @@ export { default } from "next-auth/middleware";
 export async function middleware(request: NextRequest) {
     const token = await getToken({ req: request })
     const url = request.nextUrl
-    if (token && 
+    if (token &&
         (
-            url.pathname.startsWith('/sign-in')||
-            url.pathname.startsWith('/sign-up')||
-            url.pathname.startsWith('/verify')||
+            url.pathname.startsWith('/sign-in') ||
+            url.pathname.startsWith('/sign-up') ||
+            url.pathname.startsWith('/verify') ||
             url.pathname.startsWith('/')
         )
-    ){
+    ) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
-        return NextResponse.redirect(new URL('/home', request.url))
+    if (!token && url.pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/sign-in', request.url))
     }
-    export const config = {
-        matcher: [
-            '/sign-in',
-            '/sign-up',
-            '/',
-            '/dashboard/:path*',
-            '/verify/:path*'
-        ],
-    }
+    return NextResponse.next()
+}
+export const config = {
+    matcher: [
+        '/sign-in',
+        '/sign-up',
+        '/',
+        '/dashboard/:path*',
+        '/verify/:path*'
+    ],
+}
