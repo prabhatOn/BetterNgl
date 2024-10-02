e0275622e5c3d747585be0c7c6d30ecbc2214c6d
prabhatOn <prabhat@ssipmt.com>
2024-10-03 04:59:50 +0530

    error handling for forgot password and reset password

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form'; // Import SubmitHandler for typing onSubmit
 import { Button } from '@/components/ui/button';
