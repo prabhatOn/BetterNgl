ff91bcfc33915763ec83547e35aeeeed7412e79b
prabhatOn <prabhat@ssipmt.com>
2024-10-10 19:56:15 +0530

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,8 +73,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
 
         {/* Structured Data */}
         <script
