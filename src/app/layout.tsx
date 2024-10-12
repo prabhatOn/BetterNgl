f7a96e3f7b36d2115692c562ed050a9449f14fea
prabhatOn <prabhat@ssipmt.com>
2024-10-12 16:47:39 +0530

    vercel Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,16 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import AuthProvider from '../context/AuthProvider';
 import { Toaster } from '@/components/ui/toaster';
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'TBH: Real Feedback from Real People',
-  description: 'Anonymously share your thoughts, feelings, and opinions. Get real, unfiltered feedback from your friends or followers.',
-  keywords: ['anonymous feedback', 'real opinions', 'anonymous sharing', 'thoughts', 'tbh app', 'ngl alternative', 'tbh feedback', 'anonymous messaging'],
+  title: 'TBH:Feedback from Real People',
+  description: 'Anonymously share your thoughts, feelings, and opinions. Get real, unfiltered feedback from your friends or followers,NGL alternative, TBH feedback, Anonymous messaging, Anonymous sharing, Real opinions, Real feedback, Real thoughts, Real feelings, express yourself, tbh app,tbh,Tbh, tbh feedback',
+  keywords: ['anonymous feedback', 'real opinions', 'anonymous sharing', 'thoughts', 'tbh app', 'ngl alternative', 'tbh feedback', 'anonymous messaging', 'real feedback', 'real feelings', 'express yourself', 'tbh', 'tbh feedback',"ngl","ngl feedback","ngl app","ngl alternative","ngl feedback app","ngl feedback website","ngl feedback site","ngl feedback online","ngl feedback online","ngl feedback anonymous","ngl feedback real","ngl feedback real opinions","ngl feedback real thoughts","ngl feedback real feelings","ngl feedback express yourself","ngl feedback app","ngl feedback tbh","ngl feedback tbh app","ngl feedback tbh website","ngl feedback tbh site","ngl feedback tbh online","ngl feedback tbh online","ngl feedback tbh anonymous","ngl feedback tbh real","ngl feedback tbh real opinions","ngl feedback tbh real thoughts","ngl feedback tbh real feelings","ngl feedback tbh express yourself","tbh feelings","messaging",'anonymous messaging','anonymous sharing','real opinions','real feedback','real thoughts','real feelings','express yourself','tbh app','tbh','tbh feedback'],
   robots: 'index, follow',
   openGraph: {
-    title: 'TBH: Real Feedback from Real People',
+    title: 'TBH: Feedback from Real People',
     description: 'Anonymously share your thoughts, feelings, and opinions. Get real, unfiltered feedback from your friends or followers.',
     url: 'https://tbhfeedback.live',
     siteName: 'TBH Feedback',
@@ -22,18 +22,18 @@ export const metadata: Metadata = {
         url: 'https://tbhfeedback.live/favicon.png', // Make sure this is the correct path to your image
         width: 1200,
         height: 630,
-        alt: 'TBH: Real Feedback from Real People',
+        alt: 'TBH:Feedback from Real People',
       },
     ],
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'TBH: Real Feedback from Real People',
+    title: 'TBH: Feedback from Real People',
     description: 'Anonymously share your thoughts, feelings, and opinions. Get real, unfiltered feedback from your friends or followers.',
     images: [
       {
         url: 'https://tbhfeedback.live/favicon.png',
-        alt: 'TBH: Real Feedback from Real People',
+        alt: 'TBH: Feedback from Real People',
       },
     ],
   },
@@ -51,7 +51,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
     "@context": "https://schema.org",
     "@type": "WebSite",
     "url": "https://tbhfeedback.live",
-    "name": "TBH Feedback",
+    "name": "TBH : Feedback",
     "description": "Anonymously share your thoughts, feelings, and opinions. Get real, unfiltered feedback from your friends or followers.",
     "potentialAction": {
       "@type": "SearchAction",
@@ -82,6 +82,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
         />
       </head>
+      <Analytics/>
       <AuthProvider>
         <body className={inter.className}>
           {children}
